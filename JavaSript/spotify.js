425db83382747ca2f8874f1abea866f069541147
Dr4gon6 <154282339+Dr4gon6@users.noreply.github.com>
2025-05-21 19:52:33 +0200

    df

--- JavaSript/spotify.js
+++ JavaSript/spotify.js
@@ -1,3 +1,12 @@
 fetch("https://api.dqvid.dev/v1/users/769301819562000437")
   .then((response) => response.json())
-  .then((json) => console.log(json));
+  .then((json) => {
+    const dc = document.getElementById("discord");
+    console.log();
+    console.log(
+      "Display name " +
+        json.data.discord_user.display_name +
+        " , username " +
+        json.data.discord_user.username
+    );
+  });
